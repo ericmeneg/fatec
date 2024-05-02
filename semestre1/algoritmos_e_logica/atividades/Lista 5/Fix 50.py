cd158877714ad1ca7a63480f2e5a3fad3a5d5741
ericmeneg <ericvinicius.menegon@gmail.com>
2024-05-02 09:17:49 -0300

    Lista de exercícios 5 de algoritmos

---
+++ semestre1/algoritmos_e_logica/atividades/Lista 5/Fix 50.py
@@ -0,0 +1,29 @@
+#incompleto, erro múltiplos resultados (input= 1500)
+
+def verif(n):
+    if n < 0:
+        print("Digite um número válido")
+    else:
+        return True
+    
+def interv(salario_):
+    if salario_<=1500:
+        return 1
+    elif 2500>salario_>1500:
+        return 2
+    elif salario_>=2500:
+        return 3
+
+while True:
+    salario = float(input("Digite o seu salário: "))
+    if verif(salario) == True:
+        break
+if interv(salario) == 1:
+    novosalario = salario * 1.2
+    print("Seu salário com aumento é de", novosalario)
+elif interv(salario) == 2:
+    novosalario = salario * 1.1
+    print("Seu salário com aumento é de", novosalario)
+elif interv(salario) == 3:
+    novosalario = salario * 1.05
+    print("Seu salário com aumento é de", novosalario)
