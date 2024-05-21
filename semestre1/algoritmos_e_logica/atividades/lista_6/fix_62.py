18fdf413ac691ee7a83f05e4afab27ce845cac43
ericmeneg <ericvinicius.menegon@gmail.com>
2024-05-21 11:06:21 -0300

    Encerrando lista 6

--- semestre1/algoritmos_e_logica/atividades/lista_6/fix_62.py
+++ semestre1/algoritmos_e_logica/atividades/lista_6/fix_62.py
@@ -8,4 +8,5 @@ while True:
         break
 
 with open("semestre1/algoritmos_e_logica/atividades/lista_6/senhas.txt","r",encoding="utf-8") as doc:
+    print("Nome: Eric Menegon RA:1051392411019 Turma DSM 1ºSEM 2024")
     print(doc.read())
--- semestre1/algoritmos_e_logica/atividades/lista_6/fix_63.py
+++ semestre1/algoritmos_e_logica/atividades/lista_6/fix_63.py
@@ -1,5 +1,6 @@
 with open("semestre1/algoritmos_e_logica/atividades/lista_6/mensagem.txt","r", encoding="utf-8") as doc:
     texto = doc.read()
     listadepalavras = texto.split(" ")
-    print(len(texto))
-    print(listadepalavras)
+    print("Nome: Eric Menegon RA:1051392411019 Turma DSM 1ºSEM 2024")
+    print("Número de caracteres do texto:",len(texto))
+    print("Palavras contidas em mensagem.txt:",listadepalavras)
