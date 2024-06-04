2908e59c2426407357b9176d8d0b2f66bc63b5c8
ericmeneg <ericvinicius.menegon@gmail.com>
2024-06-04 08:07:21 -0300

    Finalizando a lista 7 de algoritmos e lógica

--- semestre1/algoritmos_e_logica/atividades/lista_7/fix_71.py
+++ semestre1/algoritmos_e_logica/atividades/lista_7/fix_71.py
@@ -1,6 +1,13 @@
 #ht = hora trabalhada
 #vh = valor da hora trabalhada
 #considerando acréscimo de 50% por hora extra
+def verif(valor):
+    while True:
+        if valor <= 0:
+            print("Digite um valor válido")
+            valor = int(input("Digite o valor novamente: "))
+        else:
+            return valor
 
 def calcular_pagamento(ht,vh):
     if ht <= 40:
@@ -10,7 +17,9 @@ def calcular_pagamento(ht,vh):
     return salario_
 
 valor_hora = int(input("Digite o valor a ser pago por hora de trabalho (em R$/h): "))
+valor_hora = verif(valor_hora)
 hora_trabalhada = int(input("Digite o número inteiro de horas trabalhadas no último mês: "))
+hora_trabalhada = verif(hora_trabalhada)
 salario = calcular_pagamento(hora_trabalhada,valor_hora)
 print("Nome: Eric Menegon RA:1051392411019 Turma DSM 1ºSEM 2024")
 print("O valor a ser pago para este mês é R$"+str(salario))
