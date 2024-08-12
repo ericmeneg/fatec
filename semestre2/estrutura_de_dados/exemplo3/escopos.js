bcedec6539552e115912f572ea56cfe1d87d135b
ericmeneg <ericvinicius.menegon@gmail.com>
2024-08-12 11:11:43 -0300

    Exemplos de conceitos básicos de javascript feitos em aula no dia 12 de agosto de 2024

--- semestre2/estrutura_de_dados/exemplo3/escopos.js
+++ semestre2/estrutura_de_dados/exemplo3/escopos.js
@@ -3,11 +3,11 @@ var myOtherVariable = 'global';
 function myFunction() {
     var myVariable = 'local';
     return myVariable;
-}
+};
 function myOtherFunction() {
     myOtherVariable = 'local'; // porque não usamos var, essa função atualiza a variável declarada na linha 2
     return myOtherVariable;
-}
+};
 console.log('myVariable: ' + myVariable);
 console.log('myFunction: ' + myFunction());
 console.log('myOtherVariable: ' + myOtherVariable);
---
+++ semestre2/estrutura_de_dados/exemplo4/operadores.js
@@ -0,0 +1,21 @@
+var num = 0;
+num = num + 2;
+num = num * 3;
+num = num / 2;
+num++; // adiciona 1
+num--; // subtraí 1
+num += 1;
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
+console.log('num == 1 : ' + (num == 1));
+console.log('num === 1 : ' + (num === 1));
+console.log('num != 1 : ' + (num !=1));
+console.log('num > 1 : ' + (num > 1));
+console.log('num < 1 : ' + (num < 1));
+console.log('num >= 1 : ' + (num >= 1));
+console.log('num <= 1 : ' + (num <= 1));
+console.log('true && false : ' + (true && false)); // operador lógico and
+console.log('true || false : ' + (true || false)); // operador lógico or
+console.log('!true : ' + (!true)); // operador lógico not
